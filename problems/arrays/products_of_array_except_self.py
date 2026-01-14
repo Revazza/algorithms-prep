183dcb777a9e8cd657e33060739f21fba914afe2
Sandro <sandro.revazishviliii@gmail.com>
2026-01-14 18:02:02 +0400

    Add solution for calculating products of array except self

---
+++ problems/arrays/products_of_array_except_self.py
@@ -0,0 +1,41 @@
+﻿from typing import List
+
+
+def productExceptSelf(self, nums: List[int]) -> List[int]:
+    if len(nums) == 0:
+        return []
+
+    # 3  5  2   8
+    # 80 48 120 30
+
+    # 3  5  2  8
+
+    # 1  3  15 30
+    # 80 16 8  1
+    #
+
+    left = [1] * len(nums);
+
+    i = 1;
+
+    while i < len(nums):
+        prev_left = left[i - 1]
+        left[i] = prev_left * nums[i - 1]
+        i += 1;
+
+    right = [1] * len(nums)
+
+    i = len(right) - 2;
+
+    while i >= 0:
+        prev_right = right[i + 1];
+        right[i] = prev_right * nums[i + 1]
+        i -= 1;
+
+    result = []
+    i = 0;
+    while i < len(nums):
+        result.append(left[i] * right[i])
+        i += 1;
+
+    return result
