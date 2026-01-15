063ff43f8086ea2faeb3016afd83bafc109413f2
Sandro <sandro.revazishviliii@gmail.com>
2026-01-15 07:49:54 +0400

    Add solution for container with most water using two-pointer technique with time and space complexity comments

---
+++ problems/two_pointers/container_with_most_water.py
@@ -0,0 +1,19 @@
+﻿from typing import List
+
+
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        # Time Complexity O(n)
+        # Space Complexity O(1)
+        l = 0
+        r = len(heights) - 1
+        max_area = 0
+        while l < r:
+            width = r - l
+            max_area = max(max_area, min(heights[l], heights[r]) * width)
+            if heights[l] < heights[r]:
+                l += 1
+            else:
+                r -= 1
+
+        return max_area
