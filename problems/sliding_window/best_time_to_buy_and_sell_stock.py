f638a88cdff7b7401b23bb106f7687ce5ffda0c5
Sandro <sandro.revazishviliii@gmail.com>
2026-04-15 14:48:51 +0400

    Add new solutions

---
+++ problems/sliding_window/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,21 @@
+﻿from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        # [5,1,5,6,7,1,10]
+        left = 0
+        right = 0
+        max_profit = 0
+
+        while right < len(prices):
+            profit = prices[right] - prices[left]
+            max_profit = max(profit, max_profit)
+
+            if prices[left] > prices[right]:
+                left = right
+
+            right = right + 1
+
+        return max_profit
---
+++ problems/sliding_window/longest_substring_without_repeating_characters.py
@@ -0,0 +1,19 @@
+﻿class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        uniques = set()
+
+        left = 0
+        right = 0
+        max_length = 0
+        while right < len(s):
+            if s[right] not in uniques:
+                uniques.add(s[right])
+                right = right + 1
+            else:
+                max_length = max(len(uniques), max_length)
+                while s[right] in uniques:
+                    uniques.remove(s[left])
+                    left = left + 1
+
+        return max(max_length, len(uniques))
