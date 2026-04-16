2e445969e5c2ad5b778ac6e4061123c6372ab104
Sandro <sandro.revazishviliii@gmail.com>
2026-04-16 13:31:55 +0400

    Add permutation in string

---
+++ problems/sliding_window/permutation_in_string.py
@@ -0,0 +1,25 @@
+﻿from collections import Counter
+
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # Input: s1 = "abc", s2 = "lecaabee"
+
+        if len(s1) > len(s2):
+            return False
+
+        window = Counter(s2[0:len(s1)])
+        s1_count = Counter(s1)
+
+        if s1_count == window:
+            return True
+
+        for i in range(len(s1), len(s2)):
+
+            window[s2[i]] += 1
+            window[s2[i - len(s1)]] -= 1
+
+            if s1_count == window:
+                return True
+
+        return False
