e3e3a45817df08fb91bb928720b5e7ff0977e489
Sandro <sandro.revazishviliii@gmail.com>
2026-04-16 17:54:31 +0400

    Add sliding window maximum

---
+++ problems/sliding_window/sliding_window_maximum.py
@@ -0,0 +1,26 @@
+﻿from typing import List
+from collections import deque
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+
+        #Input: nums = [8, 7, 6, 9], k = 2
+        #Output: [8, 7, 9]
+
+        result = []
+        left = 0
+        q = deque()
+
+        for right in range(len(nums)):
+            while q and nums[right] > nums[q[-1]]:
+                q.pop()
+            q.append(right)
+
+            if left > q[0]:
+                q.popleft()
+
+            if right + 1 >= k:
+                result.append(nums[q[0]])
+            right += 1
+
+        return result
