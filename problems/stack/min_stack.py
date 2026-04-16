e9e704f6a74f74f56a4aa95fb70aee22b919c624
Sandro <sandro.revazishviliii@gmail.com>
2026-04-16 18:33:14 +0400

    Add stack folder

---
+++ problems/stack/min_stack.py
@@ -0,0 +1,33 @@
+﻿class MinStack:
+
+    # list - [1, 2, 0] - add
+    # max stack - [2, 2, 0] -
+    # min stack - [0, 0, 0]
+
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+        if not self.min_stack:
+            self.min_stack.append(val)
+        else:
+            if self.getMin() > val:
+                self.min_stack.append(val)
+            else:
+                self.min_stack.append(self.getMin())
+
+        pass
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
---
+++ problems/stack/valid_parentheses.py
@@ -0,0 +1,26 @@
+﻿from inspect import stack
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        pairs = {
+            '(': ')',
+            '{': '}',
+            '[': ']'
+        }
+
+        left = 0
+        stack = []
+
+        for char in s:
+            if char in pairs:
+                stack.append(char)
+            else:
+                if len(stack) == 0:
+                    return False;
+                c = stack.pop()
+                if pairs[c] != char:
+                    return False;
+
+        return len(stack) == 0
