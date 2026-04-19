f6a2e97f27b70f22e83c38bb4c0dcd90142b962f
Sandro <sandro.revazishviliii@gmail.com>
2026-04-19 17:00:47 +0400

    Add new solutions

---
+++ problems/linked_list/linked_list_cycle_detection.py
@@ -0,0 +1,19 @@
+﻿from typing import Optional
+
+from problems.linked_list.list_node import ListNode
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
---
+++ problems/linked_list/merge_two_sorted_linked_lists.py
@@ -0,0 +1,33 @@
+﻿from typing import Optional
+
+from problems.linked_list.list_node import ListNode
+
+
+class Solution:
+    def mergeTwoLists(self, head1: Optional[ListNode], head2: Optional[ListNode]) -> Optional[ListNode]:
+
+        newHead = ListNode(0)
+        tempHead = newHead
+
+        while head1 and head2:
+
+            if head1.val <= head2.val:
+                newHead.next = head1
+                head1 = head1.next
+            else:
+                newHead.next = head2
+                head2 = head2.next
+
+            newHead = newHead.next
+
+        while head1:
+            newHead.next = head1
+            head1 = head1.next
+            newHead = newHead.next
+
+        while head2:
+            newHead.next = head2
+            head2 = head2.next
+            newHead = newHead.next
+
+        return tempHead.next
