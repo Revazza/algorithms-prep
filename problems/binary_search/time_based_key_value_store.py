96dee6bfa76dc12ba45965b6d4aabaaa5b47b4ff
Sandro <sandro.revazishviliii@gmail.com>
2026-04-19 16:10:16 +0400

    Add new solutions

---
+++ problems/binary_search/time_based_key_value_store.py
@@ -0,0 +1,39 @@
+﻿class TimeMap:
+
+    def __init__(self):
+        self.container = {}
+
+        pass
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if not key in self.container:
+            self.container[key] = []
+
+        self.container[key].append((timestamp, value))
+        pass
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.container:
+            return ""
+
+        stamps = self.container[key]
+
+        left = 0
+        right = len(stamps) - 1
+
+        while left <= right:
+            mid = (right + left) // 2
+
+            tm, val = stamps[mid]
+
+            if tm == timestamp:
+                return val
+            elif tm > timestamp:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        if left == 0:
+            return ""
+        return stamps[left - 1][1]
+
