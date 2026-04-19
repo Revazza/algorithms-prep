f2afbb0d86fb11e0a1d17f8a05b3bd4e05507261
Sandro <sandro.revazishviliii@gmail.com>
2026-04-19 16:41:25 +0400

    Add new solutions

---
+++ problems/linked_list/reverse_linked_list.py
@@ -0,0 +1,21 @@
+﻿from typing import Optional
+
+from problems.linked_list.list_node import ListNode
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        if head is None:
+            return None
+
+        newHead = ListNode(0)
+
+        while head is not None:
+            temp_next = head.next
+            temp = newHead.next
+            newHead.next = head
+            newHead.next.next = temp
+            head = temp_next
+
+        return newHead.next
