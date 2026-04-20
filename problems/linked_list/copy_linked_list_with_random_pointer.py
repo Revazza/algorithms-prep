be4b161e5036a8b10cb19c45ae2d4d7e45e3ffca
Sandro <sandro.revazishviliii@gmail.com>
2026-04-20 17:36:01 +0400

    Add new solutions

---
+++ problems/linked_list/copy_linked_list_with_random_pointer.py
@@ -0,0 +1,34 @@
+﻿from typing import Optional
+
+from problems.linked_list.list_node import ListNode
+from problems.linked_list.node import Node
+
+
+class Solution:
+    def copyRandomList(self, head: Optional[Node]) -> 'Optional[Node]':
+
+        created = {}
+
+        newHead = Node(0)
+        tempHead = newHead
+
+        while head:
+            newNode = Node(head.val)
+
+            if head in created:
+                newNode = created[head]
+            else:
+                created[head] = newNode
+
+            if head.random and head.random in created:
+                newNode.random = created[head.random]
+            else:
+                if head.random:
+                    newNode.random = Node(head.random.val)
+                    created[head.random] = newNode.random
+
+            tempHead.next = newNode
+            tempHead = tempHead.next
+            head = head.next
+
+        return newHead.next
