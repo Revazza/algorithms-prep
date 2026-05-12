8a6ab808018487f64ec8088221daf09a64c30b40
Sandro <sandro.revazishviliii@gmail.com>
2026-05-12 16:33:13 +0400

    Add new solutions

--- problems/linked_list/merge_k_sorted_linked_lists.py
+++ problems/linked_list/merge_k_sorted_linked_lists.py
@@ -43,3 +43,38 @@ class Solution:
             tail.next = right
 
         return dummy.next
+
+    def mergeKListsFirstSolution(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        nodes = {}
+
+        for node in lists:
+            head = node;
+            while head:
+                if head.val in nodes:
+                    nodes[head.val] += 1
+                else:
+                    nodes[head.val] = 1;
+                head = head.next;
+
+        min = -1000;
+        max = 1000;
+
+        node = ListNode();
+        tail = node;
+
+        while min <= max:
+            if min not in nodes:
+                min += 1
+                continue;
+
+            count = nodes[min];
+            while count != 0:
+                temp = ListNode(min);
+                tail.next = temp;
+                tail = tail.next;
+                count -= 1
+
+            min += 1
+
+        return node.next;
