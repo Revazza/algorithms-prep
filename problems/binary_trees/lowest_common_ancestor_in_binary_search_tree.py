f4c3c3870f6b218361165213454dd19c106aa0e6
Sandro <sandro.revazishviliii@gmail.com>
2026-05-12 23:09:16 +0400

    Add new solutions

---
+++ problems/binary_trees/lowest_common_ancestor_in_binary_search_tree.py
@@ -0,0 +1,16 @@
+﻿from problems.binary_trees.tree_node import TreeNode
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, first: TreeNode, second: TreeNode) -> TreeNode:
+        current = root
+
+        while current:
+            if current.val < first.val and current.val < second.val:
+                current = current.right
+            if current.val > first.val and current.val > second.val:
+                current = current.left
+            else:
+                return current
+
+        return current
