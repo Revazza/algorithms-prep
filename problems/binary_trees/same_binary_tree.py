f2491a34c11f96514f4f921bf8dcaeb045015923
Sandro <sandro.revazishviliii@gmail.com>
2026-05-12 22:17:07 +0400

    Add new solutions

---
+++ problems/binary_trees/same_binary_tree.py
@@ -0,0 +1,21 @@
+﻿from typing import Optional
+
+from problems.binary_trees.tree_node import TreeNode
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not q and not p:
+            return True
+        if p and not q:
+            return False
+        if q and not p:
+            return False
+
+        if q.val != p.val:
+            return False
+
+        is_left_same = self.isSameTree(p.left, q.left)
+        is_right_same = self.isSameTree(p.right, q.right)
+
+        return is_left_same and is_right_same
---
+++ problems/binary_trees/subtree_of_another_tree.py
@@ -0,0 +1,29 @@
+﻿from typing import Optional
+from wsgiref.util import request_uri
+
+from problems.binary_trees.tree_node import TreeNode
+
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], sub_root: Optional[TreeNode]) -> bool:
+        return self.findIsSame(root, sub_root)
+
+    def isSame(self, root: TreeNode, sub_root: TreeNode) -> bool:
+        if not root and not sub_root:
+            return True
+
+        if root and sub_root and root.val == sub_root.val:
+            return self.isSame(root.left, sub_root.left) and self.isSame(root.right, sub_root.right)
+
+        return False
+
+    def findIsSame(self, root: TreeNode, sub_root: TreeNode) -> bool:
+        if not root:
+            return False
+
+        if root.val == sub_root.val and self.isSame(root, sub_root):
+            return True
+
+        left = self.findIsSame(root.left, sub_root)
+        right = self.findIsSame(root.right, sub_root)
+
+        return left or right
