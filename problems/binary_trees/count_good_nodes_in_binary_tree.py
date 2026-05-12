e231894a4b16ab8194a8d7261501b95643e34c64
Sandro <sandro.revazishviliii@gmail.com>
2026-05-13 00:29:46 +0400

    Add new solutions

---
+++ problems/binary_trees/count_good_nodes_in_binary_tree.py
@@ -0,0 +1,19 @@
+﻿from problems.binary_trees.tree_node import TreeNode
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+
+        stack = [(root, root.val)]
+        good_nodes = 0
+        while stack:
+            (node, path_max) = stack.pop()
+
+            if node.val >= path_max:
+                good_nodes += 1
+
+            if node.left:
+                stack.append((node.left, max(path_max, node.val)))
+            if node.right:
+                stack.append((node.right, max(path_max, node.val)))
+
+        return good_nodes
