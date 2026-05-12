01078e20b991d0c7cc408100cb1bd530b6e86100
Sandro <sandro.revazishviliii@gmail.com>
2026-05-12 16:31:27 +0400

    Add new solutions

--- problems/linked_list/merge_k_sorted_linked_lists.py
+++ problems/linked_list/merge_k_sorted_linked_lists.py
@@ -4,37 +4,42 @@ from problems.linked_list.list_node import ListNode
 
 
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    def mergeKLists(self, lists):
+        return self.divide(0, len(lists) - 1, lists)
 
-        nodes = {}
+    def divide(self, start, end, lists):
+        if start > end:
+            return None
 
-        for node in lists:
-            head = node;
-            while head:
-                if head.val in nodes:
-                    nodes[head.val] += 1
-                else:
-                    nodes[head.val] = 1;
-                head = head.next;
+        if start == end:
+            return lists[start]
 
-        min = -1000;
-        max = 1000;
+        mid = (start + end) // 2
 
-        node = ListNode();
-        tail = node;
+        left = self.divide(start, mid, lists)
+        right = self.divide(mid + 1, end, lists)
 
-        while min <= max:
-            if min not in nodes:
-                min += 1
-                continue;
+        return self.merge(left, right)
 
-            count = nodes[min];
-            while count != 0:
-                temp = ListNode(min);
-                tail.next = temp;
-                tail = tail.next;
-                count -= 1
+    def merge(self, left: Optional[ListNode], right: Optional[ListNode]) -> Optional[ListNode]:
 
-            min += 1
+        dummy = ListNode()
+        tail = dummy
 
-        return node.next;
+        while left and right:
+            if left.val < right.val:
+                tail.next = left
+                left = left.next
+            else:
+                tail.next = right
+                right = right.next
+
+            tail = tail.next
+
+        if left:
+            tail.next = left
+
+        if right:
+            tail.next = right
+
+        return dummy.next
