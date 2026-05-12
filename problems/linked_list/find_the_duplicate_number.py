abf1ff42b1900d5bad172f9d8c99184459a8fb61
Sandro <sandro.revazishviliii@gmail.com>
2026-05-12 15:50:40 +0400

    Add new solutionsAdd new solutions

---
+++ problems/linked_list/find_the_duplicate_number.py
@@ -0,0 +1,13 @@
+﻿from typing import List
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+
+        slow = 0
+        fast = 0
+        while True:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return 1
