aaeafac891108145820e625bdf5ea326a62c59f0
Sandro <sandro.revazishviliii@gmail.com>
2026-05-12 19:56:41 +0400

    Add new solutions

---
+++ problems/binary_trees/diameter_of_binary_tree.py
@@ -0,0 +1,20 @@
+﻿from typing import Optional
+
+from problems.binary_trees.tree_node import TreeNode
+
+
+class Solution:
+    maxDiameter:int = 0
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.diameter(root);
+        return self.maxDiameter
+
+    def diameter(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        left = self.diameter(root.left)
+        right = self.diameter(root.right)
+        self.maxDiameter = max(left + right, self.maxDiameter)
+
+        return 1 + max(left, right)
---
+++ problems/binary_trees/maximum_depth_of_binary_tree.py
@@ -0,0 +1,23 @@
+﻿from typing import Optional
+
+from problems.binary_trees.tree_node import TreeNode
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+
+        if not root:
+            return 0
+
+        stack = [(root, 1)]
+        max_level = 1
+
+        while stack:
+            (node, level) = stack.pop()
+
+            if node.left:
+                stack.append((node.left, level + 1))
+            if node.right:
+                stack.append((node.right, level + 1))
+            max_level = max(level, max_level)
+
+        return max_level
