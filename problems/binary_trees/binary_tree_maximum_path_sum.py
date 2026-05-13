b243ba08e47363c4e702da0ede599fc7c78dd703
Sandro <sandro.revazishviliii@gmail.com>
2026-05-13 17:11:39 +0400

    Add new solutions

---
+++ problems/binary_trees/binary_tree_maximum_path_sum.py
@@ -0,0 +1,26 @@
+﻿import sys
+from typing import Optional
+
+from problems.binary_trees.tree_node import TreeNode
+
+
+class Solution:
+    def __init__(self):
+        self.max_path = -sys.maxsize
+
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+
+        def calculate(node: TreeNode) -> int:
+            if not node:
+                return 0
+
+            left_max = max(0, calculate(node.left))
+            right_max = max(0, calculate(node.right))
+
+            self.max_path = max(self.max_path, left_max + node.val + right_max)
+
+            return node.val + max(left_max, right_max)
+
+        calculate(root)
+
+        return self.max_path
