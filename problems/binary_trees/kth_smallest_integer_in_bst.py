62b4d32f009717c3641c876226ab3a15cc070685
Sandro <sandro.revazishviliii@gmail.com>
2026-05-13 15:18:20 +0400

    Add new solutions

---
+++ problems/binary_trees/kth_smallest_integer_in_bst.py
@@ -0,0 +1,25 @@
+﻿from typing import Optional, List
+
+from problems.binary_trees.tree_node import TreeNode
+
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+
+        result = []
+        self.inorderTraversal(root, result, k)
+
+        return result[k - 1]
+
+
+    def inorderTraversal(self, node: TreeNode, result: List[int], k: int):
+        if not node:
+            return
+
+        self.inorderTraversal(node.left, result, k)
+        result.append(node.val)
+        if len(result) == k:
+            return
+        self.inorderTraversal(node.right, result, k)
+
+        return
