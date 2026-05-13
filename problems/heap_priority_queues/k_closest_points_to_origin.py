49cdebc233c8cac4aa0194f316fcbad13f574fe4
Sandro <sandro.revazishviliii@gmail.com>
2026-05-13 19:11:09 +0400

    Add new solutions

---
+++ problems/heap_priority_queues/k_closest_points_to_origin.py
@@ -0,0 +1,19 @@
+﻿import heapq
+from math import sqrt
+from typing import List
+
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+
+        heap = []
+        heapq.heapify(heap)
+
+        for coordinate in points:
+            x = coordinate[0]
+            y = coordinate[1]
+            distance = sqrt(x**2 + y**2)
+
+            heapq.heappush(heap,(distance, [x, y]))
+
+        return [heapq.heappop(heap)[1] for _ in range(k)]
