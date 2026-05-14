36b175d03dbf770d6c8661a6516f9f5e4dd65739
Sandro <sandro.revazishviliii@gmail.com>
2026-05-15 00:11:29 +0400

    Add new solutions

---
+++ problems/backtracking/generate_parentheses.py
@@ -0,0 +1,27 @@
+﻿from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+
+        result = []
+        combination = []
+
+        def gather(opened, closed):
+            if opened == n and closed == n:
+                result.append("".join(map(str, combination)))
+                return
+
+            if opened < n:
+                combination.append("(")
+                gather(opened + 1, closed)
+                combination.pop()
+
+            if closed < opened:
+                combination.append(")")
+                gather(opened, closed + 1)
+                combination.pop()
+
+        gather(0, 0)
+
+        return result
