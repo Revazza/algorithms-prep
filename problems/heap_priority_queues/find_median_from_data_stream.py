89c5f2b4fcdddc6d08c170073bb0c4644612e5b6
Sandro <sandro.revazishviliii@gmail.com>
2026-05-14 17:41:20 +0400

    Add new solutions

---
+++ problems/heap_priority_queues/find_median_from_data_stream.py
@@ -0,0 +1,38 @@
+﻿import heapq
+
+
+class MedianFinder:
+
+    def __init__(self):
+        self.low = []
+        self.high = []
+
+        heapq.heapify(self.low)
+        heapq.heapify(self.high)
+        return
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.low, -num)
+
+        if len(self.low) > len(self.high) + 1:
+            low = -heapq.heappop(self.low)
+            heapq.heappush(self.high, low)
+
+        if self.high and -self.low[0] > self.high[0]:
+            high = heapq.heappop(self.high)
+            low = -heapq.heappop(self.low)
+            heapq.heappush(self.low, -high)
+            heapq.heappush(self.high, low)
+
+        if len(self.high) > len(self.low) + 1:
+            high = heapq.heappop(self.high)
+            heapq.heappush(self.low, -high)
+
+        return None
+    def findMedian(self) -> float:
+
+        if (len(self.low) +len(self.high)) % 2 == 0:
+            return (-self.low[0] + self.high[0]) / 2
+
+        return -self.low[0]
+
