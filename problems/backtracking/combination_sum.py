52170bab1f1a3f5f12d168f3ebe4a0c08e957722
Sandro <sandro.revazishviliii@gmail.com>
2026-05-14 19:19:38 +0400

    Add new solutions

---
+++ problems/backtracking/combination_sum.py
@@ -0,0 +1,25 @@
+﻿from typing import List, Counter
+
+
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        # [2,5,6,9]
+
+        nums.sort()
+        result = []
+        sets = []
+
+        def find_sums(start, total):
+            if total == target:
+                result.append(sets.copy())
+                return
+
+            for i in range(start, len(nums)):
+                if total + nums[i] > target:
+                    return
+                sets.append(nums[i])
+                find_sums(i, total + nums[i])
+                sets.pop()
+
+        find_sums(0, 0)
+        return result
---
+++ problems/backtracking/subsets.py
@@ -0,0 +1,22 @@
+﻿from typing import List
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+
+        result = []
+        subset = []
+
+        def gather(i):
+            if i == len(nums):
+                result.append(subset.copy())
+                return
+
+            subset.append(nums[i])
+            gather(i + 1)
+
+            subset.pop()
+            gather(i + 1)
+
+        gather(0)
+        return result
