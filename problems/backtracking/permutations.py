5dd1e18fc2caaa455f4e4bbf943e0353f0b5edc9
Sandro <sandro.revazishviliii@gmail.com>
2026-05-14 21:03:51 +0400

    Add new solutions

---
+++ problems/backtracking/permutations.py
@@ -0,0 +1,28 @@
+﻿from typing import List
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+
+        result = []
+        combination = []
+        used = set([])
+
+        def gather():
+            if len(combination) == len(nums):
+                result.append(combination.copy())
+                return
+
+            for i in range(0, len(nums)):
+                if nums[i] in used:
+                    continue
+                used.add(nums[i])
+                combination.append(nums[i])
+                gather()
+                used.remove(nums[i])
+                combination.pop()
+
+
+
+        gather()
+        return result
