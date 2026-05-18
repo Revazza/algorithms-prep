c7cdd0e54d4d7d7072e5ac8fd0aeddde14515f99
Sandro <sandro.revazishviliii@gmail.com>
2026-05-18 19:24:58 +0400

    Add new solutions

---
+++ problems/backtracking/letter_combinations_of_a_phone_number.py
@@ -0,0 +1,42 @@
+﻿from typing import List
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+
+        if len(digits) == 0:
+            return []
+
+        result = []
+        combination = []
+
+        def calculate(curr):
+            if curr == len(digits):
+                result.append("".join(combination))
+                return
+
+            letters = get_letters(digits[curr])
+            for i in range(len(letters)):
+                combination.append(letters[i])
+                calculate(curr + 1)
+                combination.pop()
+
+            return
+
+        def get_letters(digit)-> str:
+            letters = {
+                "2":"abc",
+                "3":"def",
+                "4":"ghi",
+                "5":"jkl",
+                "6":"mno",
+                "7":"pqrs",
+                "8":"tuv",
+                "9":"wxyz",
+            }
+            return letters[digit]
+
+        calculate(0)
+        return result
+
+
