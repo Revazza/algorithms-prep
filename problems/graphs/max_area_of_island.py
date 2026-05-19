ce9ce3de0d101cfa8aa6963d5bfe44f0cc717d9f
Sandro <sandro.revazishviliii@gmail.com>
2026-05-19 21:41:54 +0400

    Add new solutions

---
+++ problems/graphs/max_area_of_island.py
@@ -0,0 +1,28 @@
+﻿from typing import List
+
+
+class Solution:
+
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+        def dfs(row, col):
+            if row < 0 or col < 0 or row == len(grid) or col == len(grid[0]) or grid[row][col] == 0:
+                return 0
+
+            grid[row][col] = 0
+            area = 1
+            for dr, dc in directions:
+                area += dfs(row + dr, col + dc)
+
+            return area
+
+        max_area = 0
+
+        for r in range(len(grid)):
+            for c in range(len(grid[r])):
+                scope_area = dfs(r, c)
+                max_area = max(max_area, scope_area)
+
+        return max_area
