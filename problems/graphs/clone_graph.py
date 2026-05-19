3fb2ff09c124e459b87953d1ea6e1ca0399babc1
Sandro <sandro.revazishviliii@gmail.com>
2026-05-19 22:35:31 +0400

    Add new solutions

---
+++ problems/graphs/clone_graph.py
@@ -0,0 +1,31 @@
+﻿from collections import deque
+from typing import Optional
+
+
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+class Solution:
+    def cloneGraph(self, start_node: Optional['Node']) -> Optional['Node']:
+        if not start_node:
+            return start_node
+
+        queue = deque([start_node])
+        clones = dict()
+        clones[start_node] = Node(start_node.val)
+
+        while queue:
+            node = queue.popleft()
+            clone = clones[node]
+
+            for neighbor in node.neighbors:
+                if neighbor not in clones:
+                    clones[neighbor] = Node(neighbor.val)
+                    queue.append(neighbor)
+                clone.neighbors.append(clones[neighbor])
+
+
+        return clones[start_node]
