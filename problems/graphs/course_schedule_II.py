88a806dd9e1e4909a41cbdb0d809892e69295790
Sandro <sandro.revazishviliii@gmail.com>
2026-05-21 19:41:53 +0400

    Add new solutions

---
+++ problems/graphs/course_schedule_II.py
@@ -0,0 +1,37 @@
+﻿from typing import List
+
+
+class Solution:
+    def findOrder(self, num_courses: int, prerequisites: List[List[int]]) -> List[int]:
+        courses = dict()
+        for i in range(num_courses):
+            courses[i] = []
+
+        for c, p in prerequisites:
+            courses[c].append(p)
+
+        visited = set()
+        finished = []
+
+        def fill_finished(course) -> bool:
+            if course in visited:
+                return False
+            if course in finished:
+                return True
+
+            visited.add(course)
+            prs = courses[course]
+
+            for pr in prs:
+                if not fill_finished(pr):
+                    return False
+
+            finished.append(course)
+            visited.remove(course)
+            return True
+
+        for c in range(num_courses):
+            if not fill_finished(c):
+                return []
+
+        return finished
