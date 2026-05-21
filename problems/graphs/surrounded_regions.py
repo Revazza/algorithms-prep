4d17fb0d56bed4c2c31f71b501f1fe44da868e7b
Sandro <sandro.revazishviliii@gmail.com>
2026-05-21 11:53:31 +0400

    Add new solutions

---
+++ problems/graphs/surrounded_regions.py
@@ -0,0 +1,44 @@
+﻿from operator import and_
+from typing import List
+
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+
+        if len(board) == 0:
+            return
+
+        ROW = len(board)
+        COL = len(board[0])
+
+        def dfs(row, col):
+            if row < 0 or col < 0 or row == ROW or col == COL or board[row][col] != 'O':
+                return
+
+            board[row][col] = 'T'
+            dfs(row - 1, col)
+            dfs(row + 1, col)
+            dfs(row, col - 1)
+            dfs(row, col + 1)
+
+        for r in range(ROW):
+            if board[r][0] == 'O':
+                dfs(r,0)
+            if board[r][COL - 1] == 'O':
+                dfs(r,COL - 1)
+
+        for c in range(COL):
+            if board[0][c] == 'O':
+                dfs(0, c)
+            if board[ROW - 1][c] == 'O':
+                dfs(ROW - 1, c)
+
+        for r in range(ROW):
+            for c in range(COL):
+                if board[r][c] == 'T':
+                    board[r][c] = 'O'
+                else:
+                    board[r][c] = 'X'
+
+
+        return
