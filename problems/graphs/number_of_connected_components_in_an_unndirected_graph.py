f3d65549b3d5c02870323c6574e4e23960b5b21a
Sandro <sandro.revazishviliii@gmail.com>
2026-05-28 21:54:17 +0400

    Add new solutions

---
+++ problems/graphs/number_of_connected_components_in_an_unndirected_graph.py
@@ -0,0 +1,37 @@
+﻿from typing import List
+
+
+class Solution:
+    def countComponents(self, some_N: int, edges: List[List[int]]) -> int:
+        graph = dict()
+        for i in range(some_N):
+            graph[i] = []
+
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+
+        seen = set()
+
+        def dfs(start_node) -> int:
+            if start_node in seen:
+                return 0
+            stack = [start_node]
+
+            while stack:
+                node = stack.pop()
+                seen.add(node)
+                neighs = graph[node]
+
+                for nei in neighs:
+                    if nei in seen:
+                        continue
+                    stack.append(nei)
+
+            return 1
+
+        count = 0
+        for u in graph:
+            count += dfs(u)
+
+        return count
