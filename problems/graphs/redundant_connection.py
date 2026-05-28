898a4fa045191ca9228f4de1d699bb7bdadee895
Sandro <sandro.revazishviliii@gmail.com>
2026-05-28 23:17:59 +0400

    Add new solutions

---
+++ problems/graphs/redundant_connection.py
@@ -0,0 +1,44 @@
+﻿from typing import List
+
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+
+        graph = dict()
+        for i in range(len(edges)):
+            graph[i + 1] = []
+
+        def has_cycle(start):
+
+            visited = set()
+            stack = [(start, -1)]
+
+            while stack:
+                node, parent = stack.pop()
+
+                if node in visited:
+                    continue
+
+                visited.add(node)
+
+                for nei in graph[node]:
+
+                    if nei == parent:
+                        continue
+
+                    if nei in visited:
+                        return True
+
+                    stack.append((nei, node))
+
+            return False
+
+        for u, v in edges:
+
+            graph[u].append(v)
+            graph[v].append(u)
+
+            if has_cycle(u):
+                return [u, v]
+
+        return []
