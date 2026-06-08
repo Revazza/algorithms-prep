4b7a156f85465660f0701e385b33e0a2d307f6d7
Sandro <sandro.revazishviliii@gmail.com>
2026-06-08 21:00:20 +0400

    Add new solutions

---
+++ problems/IdkYet/climbing_stairs.py
@@ -0,0 +1,14 @@
+﻿class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        dp = [0] * (n + 1)
+        dp[-1] = 1
+        dp[-2] = 1
+
+        n -= 2
+
+        while n >= 0:
+            dp[n] = dp[n + 1] + dp[n + 2]
+            n -= 1
+
+        return dp[0]
