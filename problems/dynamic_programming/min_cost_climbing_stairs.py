47773cfeb6329c0a2529f4db0364dfd022a21f28
Sandro <sandro.revazishviliii@gmail.com>
2026-06-08 22:54:41 +0400

    Add new solutions

---
+++ problems/dynamic_programming/min_cost_climbing_stairs.py
@@ -0,0 +1,24 @@
+﻿import sys
+from typing import List
+
+
+class Solution:
+    def minCostClimbingStairs(self, costs: List[int]) -> int:
+
+        cache = {}
+
+        def calc_cost(i):
+            if i >= len(costs):
+                return 0
+
+            if i in cache:
+                return cache[i]
+
+            cache[i] = costs[i] + min(
+                calc_cost(i + 1),
+                calc_cost(i + 2)
+            )
+
+            return cache[i]
+
+        return min(calc_cost(0), calc_cost(1))
