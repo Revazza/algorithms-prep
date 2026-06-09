a0314e765e1302f887c216ed71df8c0e77fad5f6
Sandro <sandro.revazishviliii@gmail.com>
2026-06-09 23:21:59 +0400

    Add new solutions

--- problems/dynamic_programming/house_robber.py
+++ problems/dynamic_programming/house_robber.py
@@ -2,6 +2,42 @@
 
 
 class Solution:
+
+    def rob(self, nums: List[int]) -> int:
+
+        if len(nums) == 1:
+            return nums[0]
+
+        back_previous = nums[0]
+        previous = max(nums[0], nums[1])
+        result = max(back_previous, previous)
+
+        for i in range(2, len(nums)):
+            result = max(nums[i] + back_previous, previous)
+            back_previous = previous
+            previous = result
+
+        return result
+
+    '''
+    def rob(self, nums: List[int]) -> int:
+
+        if len(nums) == 1:
+            return nums[0]
+
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(dp[0], nums[1])
+        # 1 2 3 1
+        # 4 1 3 1
+
+        for i in range(2, len(nums)):
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+
+        return dp[-1]
+    '''
+
+    '''
     def rob(self, nums: List[int]) -> int:
 
         dp = dict()
@@ -21,3 +57,4 @@ class Solution:
             return dp[i]
 
         return rob_house(0)
+    '''
