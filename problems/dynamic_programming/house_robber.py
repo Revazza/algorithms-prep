97005268cbd38496fcae6e0c4eb21bcfaa600fd8
Sandro <sandro.revazishviliii@gmail.com>
2026-06-09 11:13:31 +0400

    Add new solutions

---
+++ problems/dynamic_programming/house_robber.py
@@ -0,0 +1,23 @@
+﻿from typing import List
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        dp = dict()
+
+        def rob_house(i):
+            if i >= len(nums):
+                return 0
+
+            if i in dp:
+                return dp[i]
+
+            robbed = nums[i] + rob_house(i + 2)
+            not_robbed = rob_house(i + 1)
+
+            dp[i] = max(robbed, not_robbed)
+
+            return dp[i]
+
+        return rob_house(0)
