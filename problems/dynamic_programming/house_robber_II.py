737693fcc72ee32e26426d7edc81cecc3257e640
Sandro <sandro.revazishviliii@gmail.com>
2026-06-15 16:54:59 +0400

    Add new solutions

--- problems/dynamic_programming/house_robber_II.py
+++ problems/dynamic_programming/house_robber_II.py
@@ -1,6 +1,24 @@
 ﻿from typing import List
 
 class Solution:
+
+    def rob(self, nums: List[int]) -> int:
+
+        return max(nums[0], self.rob_house(nums[1:]), self.rob_house(nums[:-1]))
+
+    def rob_house(self, nums):
+        rob1 = 0
+        rob2 = 0
+
+        for n in nums:
+            result = max(n + rob1, rob2)
+            rob1 = rob2
+            rob2 = result
+
+        return rob2
+
+
+    '''
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
@@ -17,3 +35,4 @@ class Solution:
             return dp[i]
 
         return max(rob_house(0, len(nums) - 1,dict()), rob_house(1, len(nums), dict()))
+    '''
