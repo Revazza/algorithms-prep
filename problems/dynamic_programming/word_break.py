fc26f6cbb1b3f827a96fe2c3c3474a638f9c1755
Sandro <sandro.revazishviliii@gmail.com>
2026-06-17 14:18:05 +0400

    Add new solutions

---
+++ problems/dynamic_programming/word_break.py
@@ -0,0 +1,24 @@
+﻿from typing import List
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+
+        cache = dict()
+
+        def doesBreak(i):
+            if i == len(s):
+                return True
+
+            if i in cache:
+                return cache[i]
+
+            for w in wordDict:
+                if s.startswith(w, i):
+                    if doesBreak(i + len(w)):
+                        return True
+
+            cache[i] = False
+            return False
+
+        return doesBreak(0)
