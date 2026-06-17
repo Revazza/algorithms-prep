dfbfbdde8427a7fabc9e0413996d858e8482ec12
Sandro <sandro.revazishviliii@gmail.com>
2026-06-17 18:10:29 +0400

    Add new solutions

--- problems/dynamic_programming/partition_equal_subset_sum.py
+++ problems/dynamic_programming/partition_equal_subset_sum.py
@@ -7,6 +7,30 @@ class Solution:
             return False
 
         total_sum = sum(nums)
+        if total_sum % 2:
+            return False
+
+        target = total_sum // 2
+        possible = {0}
+
+        for n in nums:
+            sums = set()
+            for s in possible:
+                if s + n == target:
+                    return True
+                sums.add(s + n)
+            possible = possible.union(sums)
+
+        return target in possible
+
+    '''
+    def canPartition(self, nums: List[int]) -> bool:
+        if len(nums) < 2:
+            return False
+
+        total_sum = sum(nums)
+        if total_sum % 2 != 0:
+            return False
 
         def canItReally(curr_index, total):
             if total_sum / 2 == total:
@@ -19,3 +43,4 @@ class Solution:
                     canItReally(curr_index + 1, total))
 
         return canItReally(0, 0)
+    '''
