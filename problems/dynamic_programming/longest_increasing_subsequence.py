aa40890ed3af8985a310e47f32c5c197c1fbe0e1
Sandro <sandro.revazishviliii@gmail.com>
2026-06-17 16:29:51 +0400

    Add new solutions

---
+++ problems/dynamic_programming/longest_increasing_subsequence.py
@@ -0,0 +1,22 @@
+﻿from typing import List
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+
+        dp = {i: 1 for i in range(len(nums))}
+
+        max_length = 1
+
+        for i in range(len(nums) - 1, -1, -1):
+            temp_length = 1
+            for j in range(i + 1, len(nums)):
+                if nums[i] >= nums[j]:
+                    continue
+                temp_length = max(temp_length, 1 + dp[j])
+
+            max_length = max(temp_length, max_length)
+            dp[i] = temp_length
+
+        return max_length
