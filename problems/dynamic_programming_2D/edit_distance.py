ea3137c2bb20d4c7e4479ac1ff30502a370f4739
Sandro <sandro.revazishviliii@gmail.com>
2026-06-23 18:54:20 +0400

    Add new solutions

--- problems/dynamic_programming_2D/edit_distance.py
+++ problems/dynamic_programming_2D/edit_distance.py
@@ -2,6 +2,31 @@
 
 
 class Solution:
+
+    def minDistance(self, word: str, target: str) -> int:
+
+       m = len(word)
+       n = len(target)
+       dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+       for i in range(m + 1):
+           dp[i][n] = m - i
+       for i in range(n + 1):
+           dp[m][i] = n - i
+
+       for i in range(m - 1, - 1, -1):
+           for j in range(n - 1, - 1, -1):
+               if word[i] == target[j]:
+                   dp[i][j] = dp[i + 1][j + 1]
+               else:
+                   removeWays = 1 + dp[i + 1][j]
+                   replaceWays = 1 + dp[i + 1][j + 1]
+                   insertWays = 1 + dp[i][j + 1]
+                   dp[i][j] = min(removeWays, replaceWays, insertWays)
+
+       return dp[0][0]
+
+    '''
     def minDistance(self, word: str, target: str) -> int:
 
         cache = {}
@@ -29,3 +54,4 @@ class Solution:
             return cache[(i, j)]
 
         return calc(0, 0)
+    '''
