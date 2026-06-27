11134a93156013abab8c263207537e77db66709c
Sandro <sandro.revazishviliii@gmail.com>
2026-06-27 15:15:34 +0400

    Add new solutions

---
+++ problems/intervals/maximum_subarray.py
@@ -0,0 +1,19 @@
+﻿from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        if len(nums) == 0:
+            return 0
+
+        max_sub = nums[0]
+        sub = 0
+
+        for n in nums:
+            sub += n
+            max_sub = max(max_sub, sub)
+            if sub < 0:
+                sub = 0
+
+        return max_sub
