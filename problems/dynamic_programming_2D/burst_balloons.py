7bab382f72dcbc8717d4e6f3a8b9dc35065fbd1d
Sandro <sandro.revazishviliii@gmail.com>
2026-06-27 15:03:35 +0400

    Add new solutions

---
+++ problems/dynamic_programming_2D/burst_balloons.py
@@ -0,0 +1,31 @@
+﻿from typing import List
+
+
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+
+        def calc(i):
+            if i == len(nums):
+                return 0
+
+
+
+            return
+
+        def calcBurst(i, bursted):
+
+            prod = nums[i]
+
+            for l in range(i - 1, -1, -1):
+                if l not in bursted:
+                    prod *= nums[l]
+                    break
+
+            for r in range(i + 1, len(nums)):
+                if r not in bursted:
+                    prod *= nums[r]
+                    break
+
+            return prod
+
+        return calc(set())
