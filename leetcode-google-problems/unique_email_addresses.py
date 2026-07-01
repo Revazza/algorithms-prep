b7d3b292459a0363c90eb875231fcdc7a61871f4
Sandro <sandro.revazishviliii@gmail.com>
2026-07-01 18:07:10 +0400

    Add new solutions

---
+++ leetcode-google-problems/unique_email_addresses.py
@@ -0,0 +1,25 @@
+﻿class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+
+        sent = set()
+
+        for email in emails:
+
+            mid = -1
+            processed = ""
+            for i in range(len(email)):
+                if email[i] == "@":
+                    mid = i
+                    break
+                elif email[i] == ".":
+                    continue
+                elif email[i] == "+":
+                    while email[i + 1] != "@":
+                        i += 1
+                else:
+                    processed += email[i]
+
+            if len(processed) == 0:
+                continue
+
+            processed += email[mid:]
