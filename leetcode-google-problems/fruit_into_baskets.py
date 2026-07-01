ac39a12b3300e20601678aef6239e25d28edc699
Sandro <sandro.revazishviliii@gmail.com>
2026-07-01 21:02:30 +0400

    Add new solutions

---
+++ leetcode-google-problems/fruit_into_baskets.py
@@ -0,0 +1,30 @@
+﻿import sys
+from typing import List
+
+
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+
+        l = 0
+        r = 0
+        maxPicked = -sys.maxsize
+        picked = dict()
+
+        for r in range(len(fruits)):
+            picked[fruits[r]] = picked.get(fruits[r], 0) + 1
+
+            while l < r and len(picked) > 2:
+                picked[fruits[l]] -= 1
+
+                if picked[fruits[l]] == 0:
+                    del picked[fruits[l]]
+
+                l += 1
+
+            maxPicked = max(maxPicked, r - l + 1)
+
+        return maxPicked
+
+
+
+
