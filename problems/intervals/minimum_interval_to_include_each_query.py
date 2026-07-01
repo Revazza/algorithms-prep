eff967393e6b7b46bf0412f9ec57a0f8a0ee89d8
Sandro <sandro.revazishviliii@gmail.com>
2026-07-01 17:36:13 +0400

    Add new solutions

---
+++ problems/intervals/minimum_interval_to_include_each_query.py
@@ -0,0 +1,44 @@
+﻿import heapq
+import sys
+from typing import List
+
+
+class Solution:
+    def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
+
+        intervals.sort()
+        queries.sort()
+
+        result = []
+        minHeap = []
+        heapq.heapify(minHeap)
+
+        for q in queries:
+
+            for start, end in intervals:
+                if start > q:
+                    break
+                heapq.heappush(minHeap, (end - start + 1, end))
+
+
+
+        return []
+
+    '''
+    def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
+
+        result = []
+
+        for query in queries:
+
+            min_length = sys.maxsize
+            for start, end in intervals:
+                if start <= query <= end:
+                    min_length = min(min_length, end - start + 1)
+            if min_length == sys.maxsize:
+                min_length = -1
+
+            result.append(min_length)
+
+        return result
+    '''
